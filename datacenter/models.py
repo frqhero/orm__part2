b971e1f3f34319183f7d111d4a593a0d3d0e3f6d
frqhero <frqhero@gmail.com>
2023-04-10 01:13:13 +0600

    Replace artificial data with real

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,7 @@
+import re
+
 from django.db import models
+from django.utils import timezone
 
 
 class Passcard(models.Model):
@@ -28,3 +31,24 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
+
+    def get_entered_at(self):
+        entered_at = self.entered_at.strftime('%d <%m> %Y г. %H:%M')
+        pattern = r'<([^<>]+)>'
+        match = re.search(pattern, entered_at)
+        month_number = match.group(1)
+        months_list = [
+            'января', 'февраля', 'марта', 'апреля', 'мая', 'июня',
+            'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'
+        ]
+        newvalue = months_list[int(month_number) - 1]
+        oldvalue = f'<{month_number}>'
+        return entered_at.replace(oldvalue, newvalue)
+
+    def format_duration(self, timedelta):
+        return str(timedelta)[:5]
+
+    def get_duration(self):
+        now = timezone.localtime()
+        then = self.entered_at
+        return self.format_duration(now - then)
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,21 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+import re
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    visits = Visit.objects.filter(leaved_at=None)
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
+    non_closed_visits = []
+    for visit in visits:
+        non_closed_visit = {
+            'who_entered': visit.passcard,
+            'entered_at': visit.get_entered_at(),
+            'duration': visit.get_duration()
         }
-    ]
+        non_closed_visits.append(non_closed_visit)
+
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
