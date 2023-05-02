39c09cfe8c4dec62320558c48208d9c0dce5703c
frqhero <frqhero@gmail.com>
2023-05-02 14:54:24 +0600

    Elaborate on a variable names

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -5,9 +5,9 @@ from datacenter.models import Passcard
 
 def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
-    visits = passcard.visit_set.all()
+    passcard_visits = passcard.visit_set.all()
     this_passcard_visits = []
-    for visit in visits:
+    for visit in passcard_visits:
         this_passcard_visits.append(
             {
                 'entered_at': visit.get_entered_at(),
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -3,19 +3,18 @@ from django.shortcuts import render
 
 
 def storage_information_view(request):
-    visits = Visit.objects.filter(leaved_at=None)
-
-    non_closed_visits = []
-    for visit in visits:
+    active_visits = Visit.objects.filter(leaved_at=None)
+    active_visits_for_rendering = []
+    for visit in active_visits:
         non_closed_visit = {
             'who_entered': visit.passcard,
             'entered_at': visit.get_entered_at(),
             'duration': visit.get_duration(),
             'is_strange': visit.is_long(),
         }
-        non_closed_visits.append(non_closed_visit)
+        active_visits_for_rendering.append(non_closed_visit)
 
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': active_visits_for_rendering,
     }
     return render(request, 'storage_information.html', context)
