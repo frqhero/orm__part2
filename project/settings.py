7c5038adbc483eb287bcbe39e18c8588671dca11
frqhero <frqhero@gmail.com>
2023-05-14 15:27:24 +0600

    Set default allowed host as localhost

--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ DEBUG = env.bool('DEBUG', True)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['*'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['localhost'])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
